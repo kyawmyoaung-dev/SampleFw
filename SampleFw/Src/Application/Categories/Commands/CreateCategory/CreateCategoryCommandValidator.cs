b7edd86348b27d0733a3fc02e87b422989f725e2
acepiddev <acepiddev@gmail.com>
2021-01-04 01:31:36 +0630

    add fluent validation

---
+++ SampleFw/Src/Application/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+
+
+        public CreateCategoryCommandValidator(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            RuleFor(v => v.CategoryName)
+                .NotEmpty().WithMessage("Category Name is required.")
+                .MaximumLength(200).WithMessage("Category Name must not exceed 200 characters.")
+                .MustAsync(BeUniqueName).WithMessage("The specified title already exists.");
+        }
+
+        public async Task<bool> BeUniqueName(string name, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Categories
+                .AllAsync(l => l.CategoryName != name);
+        }
+    }
+}
--- SampleFw/Src/Application/DependencyInjection.cs
+++ SampleFw/Src/Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using FluentValidation;
+using Application.Common.Behaviours;
 
 namespace Application
 {
@@ -8,8 +10,9 @@ namespace Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
-
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             return services;
         }
     }
